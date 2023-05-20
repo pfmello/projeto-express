2857cc6055a0eb51328575009d290df8b616dc04
PFMello <philipmikh@gmail.com>
2023-05-20 09:28:41 -0300

    Instalado e configurado EJS

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
+const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
 const app = express();
 
+//CONFIGURANDO EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Checks that for every incoming request, should check if its for a file that can be found on this folder
+app.use(express.static("public"));
+
+//Middleware para json
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  const htmlFilePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, "views", "index.html");
+  // res.sendFile(htmlFilePath);
+
+  res.render("index");
 });
 
 app.get("/restaurants", (req, res) => {
-  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
-  res.sendFile(htmlFilePath);
+  res.render("restaurants");
 });
 
 app.get("/recommend", (req, res) => {
-  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
-  res.sendFile(htmlFilePath);
+  res.render("recommend");
+});
+
+app.post("/recommend", (req, res) => {
+  const restaurant = req.body;
+
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+
+  const restaurantes = JSON.parse(fileData);
+  restaurantes.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(restaurantes));
+  res.redirect("/confirm");
 });
 
 app.get("/about", (req, res) => {
-  const htmlFilePath = path.join(__dirname, "views", "about.html");
-  res.sendFile(htmlFilePath);
+  res.render("about");
 });
 
 app.get("/confirm", (req, res) => {
-  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
-  res.sendFile(htmlFilePath);
+  res.render("confirm");
 });
 
 app.listen(3000);
